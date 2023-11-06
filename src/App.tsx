765dfcca4acfb324271f93c9920c4fa1eec3901d
locmann <b0ne7228@gmail.com>
2023-11-06 14:12:55 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Header } from "./components/Header";
+import { Categories } from "./components/Categories";
+import { Sort } from "./components/Sort";
+import { Pizza } from "./components/Pizza";
+import "./scss/app.scss";
+import pizzas from "./assets/pizzas.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((m) => (
+              <Pizza {...m} />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const Categories = () => {
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => setActiveIndex(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
